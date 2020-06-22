605f31e166532d1fd720829545c874745676ca21
Nathan King <34531378+nathan-king@users.noreply.github.com>
2020-06-22 16:20:40 +1000

    validating answers

--- src/components/Game.js
+++ src/components/Game.js
@@ -48,7 +48,10 @@ export default class Game extends Component {
       <>
         {this.state.loading && <div id="loader" />}
         {!this.state.loading && this.state.currentQuestion && (
-          <Question question={this.state.currentQuestion} />
+          <Question
+            question={this.state.currentQuestion}
+            changeQuestion={this.changeQuestion}
+          />
         )}
       </>
     );
