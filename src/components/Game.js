b35223687a3d30a0c6f535662f8671c17b4a21cb
Nathan King <34531378+nathan-king@users.noreply.github.com>
2020-06-22 15:13:56 +1000

    Fetch data from Trivia API and display them dynamically

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import Question from "./Question";
-
-const dummyQuestion = {
-  question: "What's the best programming language?",
-  answerChoices: ["JavaScript", "Java", "C#", "Swift"],
-};
+import { loadQuestions } from "../helpers/Helper";
 
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: null,
+      currentQuestion: null,
+      loading: true,
+    };
+  }
+  async componentDidMount() {
+    try {
+      const questions = await loadQuestions();
+      console.log(questions);
+
+      this.setState({
+        questions,
+        currentQuestion: questions[0],
+        loading: false,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
     return (
       <>
-        <Question question={dummyQuestion} />
+        {this.state.loading && <div id="loader"></div>}
+        {!this.state.loading && this.state.currentQuestion && (
+          <Question question={this.state.currentQuestion} />
+        )}
       </>
     );
   }
